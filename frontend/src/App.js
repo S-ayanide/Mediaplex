a4d4f2d0adac0cf3aa1fcd8f1736ccf168c1d67c
Sayan Mondal <sayan@chaosnative.com>
2021-12-31 22:56:58 +0530

    feat: Finishing up
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { Login } from "./component";
 import Home from "./container/Home";
 
 const App = () => {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const User =
+			localStorage.getItem("user") !== "undefined"
+				? JSON.parse(localStorage.getItem("user"))
+				: localStorage.clear();
+
+		if (!User) navigate("/login");
+	}, []);
+
 	return (
 		<Routes>
 			<Route path="login" element={<Login />} />
--- frontend/src/component/Sidebar.js
+++ frontend/src/component/Sidebar.js
@@ -3,20 +3,12 @@ import { NavLink, Link } from "react-router-dom";
 import { RiHomeFill } from "react-icons/ri";
 import { IoIosArrowForward } from "react-icons/io";
 import logo from "../assets/logo_transparent.png";
-// import { categories } from "../utils/data";
+import { categories } from "../utils/data";
 
 const isNotActiveStyle =
 	"flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
 const isActiveStyle =
 	"flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize";
-
-const categories = [
-	{ name: "Animal" },
-	{ name: "Photography" },
-	{ name: "Coding" },
-	{ name: "Movies" },
-	{ name: "Other" },
-];
 
 const Sidebar = ({ closeToggle, user }) => {
 	const handleCloseSidebar = () => {
@@ -54,7 +46,7 @@ const Sidebar = ({ closeToggle, user }) => {
 							onClick={handleCloseSidebar}
 							key={category.name}
 						>
-							{/* <img src={category.image} className="w-8 h-8 rounded-full shadow-sm" /> */}
+							<img src={category.image} className="w-8 h-8 rounded-full shadow-sm" />
 							{category.name}
 						</NavLink>
 					))}
